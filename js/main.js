2620eb13444440e4c726cdb340ae9534558179a1
Pavel Elyutin <elyutin.pavel@gmail.com>
2026-09-12 22:28:45 +0300

    add gsap

--- js/main.js
+++ js/main.js
@@ -47,28 +47,71 @@ document.addEventListener("DOMContentLoaded", () => {
       991: {
         slidesPerView: 2,
         spaceBetween: 40,
-      }
-    }
+      },
+    },
   });
 
   // burger
-  const burger = document.querySelector('.burger')
-  const headerRight = document.querySelector('.header__right')
+  const burger = document.querySelector(".burger");
+  const headerRight = document.querySelector(".header__right");
+  const navLinks = document.querySelectorAll(".nav__link");
 
-  burger.addEventListener('click', function () {
-    burger.classList.toggle('active')
-    headerRight.classList.toggle('active')
-  })
+  burger.addEventListener("click", function () {
+    burger.classList.toggle("active");
+    headerRight.classList.toggle("active");
+    document.body.classList.toggle("body-hidden");
+  });
+
+  // close nav on click link
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      burger.classList.remove("active");
+      headerRight.classList.remove("active");
+      document.body.classList.remove("body-hidden");
+    });
+  });
 
   // shadow in header after scrolling
-  const header = document.querySelector('.header');
+  const header = document.querySelector(".header");
 
-  window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
-      header.classList.add('scrolled');
+      header.classList.add("scrolled");
     } else {
-      header.classList.remove('scrolled');
+      header.classList.remove("scrolled");
     }
   });
 
+  // gsap animation
+  const heroSvg = document.getElementById("hero-svg");
+
+  heroSvg.addEventListener("load", () => {
+    const docSvg = heroSvg.contentDocument;
+    const heroIcons = docSvg.querySelectorAll(".hero__svg-icon");
+
+    heroIcons.forEach((heroIcon) => {
+      moveRandomly(heroIcon);
+    });
+  });
+
+  function moveRandomly(element) {
+    const randomX = gsap.utils.random(-10, 10);
+    const randomY = gsap.utils.random(-10, 10);
+
+    const randomDuration = gsap.utils.random(1, 2);
+
+    gsap.to(element, {
+      x: randomX,
+      y: randomY,
+      duration: randomDuration,
+      ease: "sine.inOut",
+      onComplete: () => {
+        moveRandomly(element);
+      },
+    });
+  }
+
+  gsap.from(".hero__title", { x: -500, opacity: 0 });
+  gsap.from(".hero__subtitle", { x: -500, opacity: 0, duration: 0.6 });
+  gsap.from(".hero__button", { x: -500, opacity: 0, duration: 0.7 });
 });
